723d04d6a6da7b4a2639480995456035dfab5472
ShimaShakouri <89384876+ShimaShakouri@users.noreply.github.com>
2022-09-11 20:46:35 +0200

    Add files via upload

---
+++ EX-2/visualization test.py
@@ -0,0 +1,397 @@
+import numpy as np
+
+np.set_printoptions(threshold=np.inf, linewidth=300)
+#import time
+
+import pandas as pd
+from PIL import Image
+
+
+from warnings import warn
+import heapq
+
+
+
+class Node:
+    """
+    A node class for A* Pathfinding
+    """
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+    
+    def __repr__(self):
+      return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
+
+    # defining less than for purposes of heap queue
+    def __lt__(self, other):
+      return self.f < other.f
+    
+    # defining greater than for purposes of heap queue
+    def __gt__(self, other):
+      return self.f > other.f
+
+def return_path(current_node):
+    path = []
+    current = current_node
+    while current is not None:
+        path.append(current.position)
+        current = current.parent
+    return path[::-1]  # Return reversed path
+
+
+def astar(maze, start_pos, goal_pos, allow_diagonal_movement = False):
+    """
+    Returns a list of tuples as a path from the given start to the given end in the given maze
+    :param maze:
+    :param start_pos:
+    :param goal_pos:
+    :return:
+    """
+
+    # Create start and goal_pos node
+    start_node = Node(None, start_pos)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, goal_pos)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Heapify the open_list and Add the start node
+    heapq.heapify(open_list) 
+    heapq.heappush(open_list, start_node)
+
+    # Adding a stop condition
+    outer_iterations = 0
+    #max_iterations = (len(maze[0]) * len(maze) // 2)
+    max_iterations = 50000
+    # what squares do we search
+    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+        outer_iterations += 1
+
+        if outer_iterations > max_iterations:
+          # if we hit this point return the path such as it is
+          # it will not contain the destination
+          warn("giving up on pathfinding too many iterations")
+          return return_path(current_node)       
+        
+        # Get the current node
+        current_node = heapq.heappop(open_list)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            return return_path(current_node)
+
+        # Generate children
+        children = []
+        
+        for new_position in adjacent_squares: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] == -1:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            # Child is on the closed list
+            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + maze[child.position[0]][child.position[1]]
+            #child.g = 
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
+                continue
+
+            # Add the child to the open list
+            heapq.heappush(open_list, child)
+
+    warn("Couldn't get a path to destination")
+    return None
+
+class Map_Obj():
+    def __init__(self, task=1):
+        self.start_pos, self.goal_pos, self.end_goal_pos, self.path_to_map = self.fill_critical_positions(
+            task)
+        self.int_map, self.str_map = self.read_map(self.path_to_map)
+        self.tmp_cell_value = self.get_cell_value(self.goal_pos)
+        self.set_cell_value(self.start_pos, ' S ')
+        self.set_cell_value(self.goal_pos, ' G ')
+        self.tick_counter = 0
+
+    def read_map(self, path):
+        """
+        Reads maps specified in path from file, converts them to a numpy array and a string array. Then replaces
+        specific values in the string array with predefined values more suitable for printing.
+        :param path: Path to .csv maps
+        :return: the integer map and string map
+        """
+        # Read map from provided csv file
+        df = pd.read_csv(path, index_col=None,
+                         header=None)  #,error_bad_lines=False)
+        # Convert pandas dataframe to numpy array
+        data = df.values
+        # Convert numpy array to string to make it more human readable
+        data_str = data.astype(str)
+        # Replace numeric values with more human readable symbols
+        data_str[data_str == '-1'] = ' # '
+        data_str[data_str == '1'] = ' . '
+        data_str[data_str == '2'] = ' , '
+        data_str[data_str == '3'] = ' : '
+        data_str[data_str == '4'] = ' ; '
+        return data, data_str
+
+    def fill_critical_positions(self, task):
+        """
+        Fills the important positions for the current task. Given the task, the path to the correct map is set, and the
+        start, goal and eventual end_goal positions are set.
+        :param task: The task we are currently solving
+        :return: Start position, Initial goal position, End goal position, path to map for current task.
+        """
+        
+        
+        
+        if task == 1:
+            start_pos = [27, 18]
+            goal_pos = [40, 32]
+            end_goal_pos = goal_pos
+            path_to_map = 'Samfundet_map_1.csv'
+        elif task == 2:
+            start_pos = [40, 32]
+            goal_pos = [8, 5]
+            end_goal_pos = goal_pos
+            path_to_map = 'Samfundet_map_1.csv'
+        elif task == 3:
+            start_pos = [28, 32]
+            goal_pos = [6, 32]
+            end_goal_pos = goal_pos
+            path_to_map = 'Samfundet_map_2.csv'
+        elif task == 4:
+            start_pos = [28, 32]
+            goal_pos = [6, 32]
+            end_goal_pos = goal_pos
+            path_to_map = 'Samfundet_map_Edgar_full.csv'
+        elif task == 5:
+            start_pos = [14, 18]
+            goal_pos = [6, 36]
+            end_goal_pos = [6, 7]
+            path_to_map = 'Samfundet_map_2.csv'
+
+        return start_pos, goal_pos, end_goal_pos, path_to_map
+
+    def get_cell_value(self, pos):
+        return self.int_map[pos[0], pos[1]]
+
+    def get_goal_pos(self):
+        return self.goal_pos
+
+    def get_start_pos(self):
+        return self.start_pos
+
+    def get_end_goal_pos(self):
+        return self.end_goal_pos
+
+    def get_maps(self):
+        # Return the map in both int and string format
+        return self.int_map, self.str_map
+
+    def move_goal_pos(self, pos):
+        """
+        Moves the goal position towards end_goal position. Moves the current goal position and replaces its previous
+        position with the previous values for correct printing.
+        :param pos: position to move current_goal to
+        :return: nothing.
+        """
+        tmp_val = self.tmp_cell_value
+        tmp_pos = self.goal_pos
+        self.tmp_cell_value = self.get_cell_value(pos)
+        self.goal_pos = [pos[0], pos[1]]
+        self.replace_map_values(tmp_pos, tmp_val, self.goal_pos)
+
+    def set_cell_value(self, pos, value, str_map=True):
+        if str_map:
+            self.str_map[pos[0], pos[1]] = value
+        else:
+            self.int_map[pos[0], pos[1]] = value
+
+    def print_map(self, map_to_print):
+        # For every column in provided map, print it
+        for column in map_to_print:
+            print(column)
+
+    def pick_move(self):
+        """
+        A function used for moving the goal position. It moves the current goal position towards the end_goal position.
+        :return: Next coordinates for the goal position.
+        """
+        if self.goal_pos[0] < self.end_goal_pos[0]:
+            return [self.goal_pos[0] + 1, self.goal_pos[1]]
+        elif self.goal_pos[0] > self.end_goal_pos[0]:
+            return [self.goal_pos[0] - 1, self.goal_pos[1]]
+        elif self.goal_pos[1] < self.end_goal_pos[1]:
+            return [self.goal_pos[0], self.goal_pos[1] + 1]
+        else:
+            return [self.goal_pos[0], self.goal_pos[1] - 1]
+
+    def replace_map_values(self, pos, value, goal_pos):
+        """
+        Replaces the values in the two maps at the coordinates provided with the values provided.
+        :param pos: coordinates for where we want to change the values
+        :param value: the value we want to change to
+        :param goal_pos: The coordinate of the current goal
+        :return: nothing.
+        """
+        if value == 1:
+            str_value = ' . '
+        elif value == 2:
+            str_value = ' , '
+        elif value == 3:
+            str_value = ' : '
+        elif value == 4:
+            str_value = ' ; '
+        else:
+            str_value = str(value)
+        self.int_map[pos[0]][pos[1]] = value
+        self.str_map[pos[0]][pos[1]] = str_value
+        self.str_map[goal_pos[0], goal_pos[1]] = ' G '
+
+    def tick(self):
+        """
+        Moves the current goal position every 4th call if current goal position is not already at the end_goal position.
+        :return: current goal position
+        """
+        # For every 4th call, actually do something
+        if self.tick_counter % 4 == 0:
+            # The end_goal_pos is not set
+            if self.end_goal_pos is None:
+                return self.goal_pos
+            # The current goal is at the end_goal
+            elif self.end_goal_pos == self.goal_pos:
+                return self.goal_pos
+            else:
+                # Move current goal position
+                move = self.pick_move()
+                self.move_goal_pos(move)
+                #print(self.goal_pos)
+        self.tick_counter += 1
+
+        return self.goal_pos
+
+    def set_start_pos_str_marker(self, start_pos, map):
+        # Attempt to set the start position on the map
+        if self.int_map[start_pos[0]][start_pos[1]] == -1:
+            self.print_map(self.str_map)
+            print('The selected start position, ' + str(start_pos) +
+                  ' is not a valid position on the current map.')
+            exit()
+        else:
+            map[start_pos[0]][start_pos[1]] = ' S '
+
+    def set_goal_pos_str_marker(self, goal_pos, map):
+        # Attempt to set the goal position on the map
+        if self.int_map[goal_pos[0]][goal_pos[1]] == -1:
+            self.print_map(self.str_map)
+            print('The selected goal position, ' + str(goal_pos) +
+                  ' is not a valid position on the current map.')
+            exit()
+        else:
+            map[goal_pos[0]][goal_pos[1]] = ' G '
+
+    def show_map(self, map=None):
+        """
+        A function used to draw the map as an image and show it.
+        :param map: map to use
+        :return: nothing.
+        """
+        # If a map is provided, set the goal and start positions
+        if map is not None:
+            self.set_start_pos_str_marker(self.start_pos, map)
+            self.set_goal_pos_str_marker(self.goal_pos, map)
+        # If no map is provided, use string_map
+        else:
+            map = self.str_map
+
+        # Define width and height of image
+        width = map.shape[1]
+        height = map.shape[0]
+        # Define scale of the image
+        scale = 20
+        # Create an all-yellow image
+        image = Image.new('RGB', (width * scale, height * scale),
+                          (255, 255, 0))
+        # Load image
+        pixels = image.load()
+
+        # Define what colors to give to different values of the string map (undefined values will remain yellow, this is
+        # how the yellow path is painted)
+        colors = {
+            ' # ': (211, 33, 45),
+            ' . ': (215, 215, 215),
+            ' , ': (166, 166, 166),
+            ' : ': (96, 96, 96),
+            ' ; ': (36, 36, 36),
+            ' S ': (255, 0, 255),
+            ' G ': (0, 128, 255),
+            ' O ': (255, 165, 0)
+        }
+        
+        
+        with open("Samfundet_map_2.csv") as file_name:
+            maze = np.loadtxt(file_name, delimiter=",")
+
+    
+        #start_pos = (40, 32)
+        #goal_pos = (8 , 5)
+        start_pos = [28, 32]
+        goal_pos = [6, 32]
+        
+        founded_path = astar(maze, start_pos, goal_pos)
+        
+        for m in range (1 , (len(founded_path) - 1)):
+                map[founded_path[m][0]][founded_path[m][1]] = ' O '
+        
+        
+        # Go through image and set pixel color for every position
+        for y in range(height):
+            for x in range(width):
+                if map[y][x] not in colors: continue
+                for i in range(scale):
+                    for j in range(scale):
+                        pixels[x * scale + i,
+                               y * scale + j] = colors[map[y][x]]
+
+        
+        # Show image
+        image.show()
